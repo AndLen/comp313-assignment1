f9b8a8b4ba4c487c33f87da2acbc55058804cecf
Andrew Lensen <andrewlensen@gmail.com>
2014-04-02 19:29:40 +1300

    Initial Commit

---
+++ New Unity Project 1/Assets/_Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+	System.Random random = new System.Random();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public float speed = 100f;
+    
+       void Update ()
+       {
+		   Vector3 v = new Vector3(random.Next(0,2),random.Next(0,2),random.Next(0,2));
+		   Debug.Log(v);
+           transform.Rotate(v, Space.World);		   
+       }
+}
