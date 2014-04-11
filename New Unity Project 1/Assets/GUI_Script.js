6cc2e01517435d36e9530178de2a2ef512697749
Andrew Lensen <andrewlensen@gmail.com>
2014-04-11 19:32:32 +1200

    much smarter AI (2 of them!)

--- New Unity Project 1/Assets/GUI_Script.js
+++ New Unity Project 1/Assets/GUI_Script.js
@@ -4,39 +4,59 @@ var score: int;
 var scoreText: GUIText;
 var pickupTexture: Texture;
 var additionalCustomBehaviour: AdditionalCustomBehaviour;
-function Start () {
-        score = 0;
-        updateScore(0);
+private var lastItemRemovedTime: float;
+
+function Start() {
+    score = 0;
+    updateScore(0);
+    lastItemRemovedTime = Time.time;
 
 }
 
-function Update () {
+function Update() {
 
 }
 
-function OnGUI () {
-GUILayout.BeginArea(Rect(10,10,250,200));
-    for(var i = 0; i < score; i++){
+function OnGUI() {
+    GUILayout.BeginArea(Rect(10, 10, 250, 200));
+    for (var i = 0; i < score; i++) {
         //Yeah yeah, ugly
-        if(i==0 || i == 4 || i==8){
+        if (i == 0 || i == 4 || i == 8) {
             GUILayout.BeginHorizontal();
         }
-        if(GUILayout.Button(pickupTexture,GUILayout.Height(50),GUILayout.Width(50))){
-            updateScore(-1);
-            additionalCustomBehaviour.unhidePickup();
+        if (GUILayout.Button(pickupTexture, GUILayout.Height(50), GUILayout.Width(50))) {
+            removeItem(false);
+
         }
-        if(i==3 || i == 7 || i == 11){
+        if (i == 3 || i == 7 || i == 11) {
             GUILayout.EndHorizontal();
         }
 
     }
-    
+
     GUILayout.EndArea();
 
 }
 
-function updateScore(addAmount: int){
-    score+=addAmount;
-        scoreText.text = "Score: " + score;
+function updateScore(addAmount: int) {
 
+    score += addAmount;
+    scoreText.text = "Score: " + score;
+
+}
+
+function removeItem(forcefullyRemoved: boolean) {
+    if (forcefullyRemoved) {
+        if (Time.time - lastItemRemovedTime > 1) {
+            updateScore(-1);
+            additionalCustomBehaviour.unhidePickup();
+
+        }
+        lastItemRemovedTime = Time.time;
+
+
+    } else {
+        updateScore(-1);
+        additionalCustomBehaviour.unhidePickup();
+    }
 }
